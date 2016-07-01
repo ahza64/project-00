b7c7297248ee0a9b38d6f1a4ccbf0420388a0a06
ahza64 <ahza64@yahoo.com>
2016-07-01 16:38:55 -0700

    clean more code, set indentation correctly

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//var player1, player2;
+
 $(document).on('ready', function() {
   console.log('JS is loaded!');
 
@@ -8,8 +8,8 @@ $(document).on('ready', function() {
   var countPlayers = 3;//put input here
   var Nums = 0;
 
+  // create players
   numPlayers(countPlayers);
-
 
   function numPlayers(num){
     var playerNum = 1;
@@ -18,11 +18,9 @@ $(document).on('ready', function() {
       playersArr[s] = new Player(randString(), randString(), playerNum, Nums);
       playerNum++;
     }
-    //console.log(playersArr);
   }
 
   //reset keypress counter for each player (set marker back to start position)
-
 
   //generates random Avater, player image
   function randAvatar(input){
@@ -41,7 +39,7 @@ $(document).on('ready', function() {
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
     for( var i = 0; i < 5; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
   }
 
@@ -54,8 +52,8 @@ $(document).on('ready', function() {
     return playmouth;
   }
 
-//this is where it all starts!!!!
-setUpGame();
+  //this is where it all starts!!!!
+  setUpGame();
 
   function numSpaces(){
     var spaces = '';
@@ -78,84 +76,84 @@ setUpGame();
     this.playerColor = color;
     this.playerLetter = letter;
     this.buildRow = function (){
-                          $('#board').append('<div class="size '+this.playerColor+
-                            '"><img class="avatar" id="' + this.playerColor + '" src=https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/8/005/079/233/062b1d4.jpg> ' +
-                            playerPos + '</div>');
-                          this.buildString();
-                          this.keyStroke(0);
-                    };//buildRow
+      $('#board').append('<div class="size '+this.playerColor+
+      '"><img class="avatar" id="' + this.playerColor + '" src=https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/8/005/079/233/062b1d4.jpg> ' +
+      playerPos + '</div>');
+      this.buildString();
+      this.keyStroke(0);
+    };//buildRow
     this.buildString = function (){
-                            for(var t = 0; t < boardLength; t++){
-                              $('.'+this.playerColor).append('<div class="inline clear ' +
-                                this.playerLetter + '" id="' + letter + t + '">_______</div>');
-                            }//for
-                            //finish line
-                            $('.'+this.playerColor).append('<div class="inline">|</div>');
-                        };//buildString
+      for(var t = 0; t < boardLength; t++){
+        $('.'+this.playerColor).append('<div class="inline clear ' +
+        this.playerLetter + '" id="' + letter + t + '">_______</div>');
+      }//for
+      //finish line
+      $('.'+this.playerColor).append('<div class="inline">|</div>');
+    };//buildString
 
     //building the keypress automatiaclly
     this.keyStroke = function key(pressNum){
-                        $(window).on("keyup", function handleKeypress(event) {
-                          if(event.keyCode === (47 + playerPos)){
-                            if(pressNum === (boardLength-1)){
-                              $('#' + letter + pressNum).text(".");
-                              $('#' + letter + (pressNum-1)).text("_______");
-                              console.log(pressNum);
-                              alert('player' + playerPos + ' has won!');
-                              $('.clear').text("_______");
-//I could not get the board to reset properly
-                              pressNum = 0;
-                              window.location.reload();
-                            }else{
-                              $('#' + letter + pressNum).append("<img class=avatar src=http://i.imgur.com/mRGbsfa.jpg>");
-                              $('#' + letter + (pressNum-1)).text("_______");
-                              console.log('#' + letter + pressNum);
-                              pressNum++;
-                            }
-                          }//if key code
-                        });//keypress
-                      };
+      $(window).on("keyup", function handleKeypress(event) {
+        if(event.keyCode === (47 + playerPos)){
+          if(pressNum === (boardLength-1)){
+            $('#' + letter + pressNum).text(".");
+            $('#' + letter + (pressNum-1)).text("_______");
+            console.log(pressNum);
+            alert('player' + playerPos + ' has won!');
+            $('.clear').text("_______");
+            //I could not get the board to reset properly
+            pressNum = 0;
+            window.location.reload();
+          }else{
+            $('#' + letter + pressNum).append("<img class=avatar src=http://i.imgur.com/mRGbsfa.jpg>");
+            $('#' + letter + (pressNum-1)).text("_______");
+            console.log('#' + letter + pressNum);
+            pressNum++;
+          }
+        }//if key code
+      });//keypress
+    };
 
     this.avatar = function() {
-                    $('#search-form').on("submit", function handleSubmit(event){
-                      event.preventDefault();
-                      //see the serialized info
-                      console.log("form serialized", $('#search-form').serialize());
+      $('#search-form').on("submit", function handleSubmit(event){
+        event.preventDefault();
+        //see the serialized info
+        console.log("form serialized", $('#search-form').serialize());
 
-                      $.ajax({
-                        method: 'GET',
-                        url: 'https://api.spotify.com/v1/search',
-                        data: $('#search-form').serialize(),
-                        success: handleSuccessCallback,
-                        error: handleErrorCallback
-                      });
+        $.ajax({
+          method: 'GET',
+          url: 'https://api.spotify.com/v1/search',
+          data: $('#search-form').serialize(),
+          success: handleSuccessCallback,
+          error: handleErrorCallback
+        });
 
-                      function handleSuccessCallback(json){
+        function handleSuccessCallback(json){
 
-                      //json.tracks.items.forEach(function(event){
-                        //var trackName = event.name;
-                        var albumUrl = json.tracks.items[0].event.album.images[0].url;
-                        //console.log(json.tracks.items[0].album.images[0].url);
-                        //var resultsHtml = ;
+          //json.tracks.items.forEach(function(event){
+          //var trackName = event.name;
+          var albumUrl = json.tracks.items[0].event.album.images[0].url;
+          //console.log(json.tracks.items[0].album.images[0].url);
+          //var resultsHtml = ;
 
-                        var source = $('#track-result-item').html();
-                        var template = Handlebars.compile(source);
-                        var resultsHtml = template({
-                          trackName: trackName,
-                          albumUrl: albumUrl
-                        });
-                        //$('#results').text("results coming soon");
-                        $('img#'+this.playerColor).attr(src, albumUrl);
-                        console.log(albumUrl);
+          var source = $('#track-result-item').html();
+          var template = Handlebars.compile(source);
+          var resultsHtml = template({
+            trackName: trackName,
+            albumUrl: albumUrl
+          });
+          //$('#results').text("results coming soon");
+          $('img#'+this.playerColor).attr(src, albumUrl);
+          console.log(albumUrl);
 
 
-                      //});//forEach
-                      }
-                      function handleErrorCallback(){
+          //});//forEach
+        }
+        function handleErrorCallback(){
 
-                      }
-                    }); //handleCallback
-                  };//movement
+        }
+      }); //handleCallback
+    };//movement
   }//Player
 
 
